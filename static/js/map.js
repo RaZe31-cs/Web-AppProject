0c03ae9f8d1d616b675a4b16be7d53da6a7d1330
RaZe31-cs <gri.dubrovskih@gmail.com>
2024-04-09 21:01:08 +0500

    Отрисовка карты

--- static/js/map.js
+++ static/js/map.js
@@ -1,19 +1,62 @@
+window.map = null
+
 initMap()
+
 
 async function initMap() {
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer} = ymaps3;
 
-    const map = new YMap(
+    window.map = new YMap(
         document.getElementById('map'),
         {
             location: {
                 center: [37.588144, 55.733842],
-                zoom: 10
+                zoom: 2
             }
         }
     );
+    window.map.addChild(new YMapDefaultSchemeLayer());
 
-    map.addChild(new YMapDefaultSchemeLayer());
+    window.map.addChild(new YMapDefaultFeaturesLayer());
+
+
+}
+
+
+async function deleteMap() {
+    alert('Карта удалена')
+    window.map.destroy();
+}
+
+
+async function changeCenter(el) {
+    const {YMapMarker} = ymaps3;
+
+    var parent = el.parentNode.parentNode;
+    // Получаем родительский div
+    var h6Element = parent.querySelector('h6');   
+    //Находим в нем h6
+    var longitude = h6Element.getAttribute('data-coord1');
+    var latitude = h6Element.getAttribute('data-coord2');
+    //Записываем атрибуты
+    alert(longitude + latitude);  
+    window.map.setLocation({
+        center: [Number(longitude), Number(latitude)],
+        zoom: 17
+      })
+
+    const content = document.createElement('section');
+    
+    const marker = new YMapMarker(
+    {
+        coordinates: [Number(longitude), Number(latitude)],
+        draggable: true
+    }
+    );
+    
+    window.map.addChild(marker);
+      
+
 }
